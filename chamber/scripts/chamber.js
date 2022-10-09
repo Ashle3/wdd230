235b2742c49cd2177abc80a8268fb80df5dd8788
Ashle3 <ashleeb3@byui.edu>
2022-10-08 22:40:53 -0500

    small viewport complete

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -9,3 +9,20 @@ datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
 function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
+
+
+try {
+    let options = {
+        year: "numeric"
+
+    }
+    document.getElementById(
+        "currentyear"
+        ).textContent = new Date().toLocaleDateString("en-US", options);
+} catch (e) {
+    alert("Error");
+}
+
+let lastModif = new Date(document.lastModified);
+
+document.getElementById("lastModified").innerHTML = lastModif
