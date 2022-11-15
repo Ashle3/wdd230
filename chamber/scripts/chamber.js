5424a3b78e83def2c543a0d4cab4473910699e18
Ashle3 <ashleeb3@byui.edu>
2022-11-14 18:07:29 -0600

    until i get help

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -64,16 +64,55 @@ let d = new Date();
 let today = d.getDate();
 
 const visitDisplay = document.querySelector("#lastvisit");
-/*help here*/
+/*help here
 let lastVisit = window.localStorage.getItem("lastvisit");
 
 if (numVisits !== 0){
     visitDisplay.textContent = numVisits;
 } else {
     visitDisplay.textContent = "This is your first visit!";
+}*/
+
+
+/* Adding the hidden date input on join page 
+
+document.querySelector("#date").innerHTML = today;*/
+
+
+/* Displaying the business directory cards from json file */
+
+const requestURL1 = 'https://ashle3.github.io/wdd230/chamber/json/data.json';
+const cards = document.querySelector(".cards");
+
+fetch(requestURL1)
+    .then(function (response) {
+        return response.json;
+    })
+    .then(function(jsonObject){
+        console.table(jsonObject);
+        const directory = jsonObject["directory"];
+        directory.forEach(displayBusinesses);
+    })
+
+function displayBusinesses(business) {
+    let card = document.createElement('section');
+    let logo = document.createElement('img');
+    let address = document.createElement('p');
+    let telephone = document.createElement('p');
+    let website = document.createElement('p');
+
+    address.textContent = `${business.address}`;
+    telephone.textContent = `${business.phone}`;
+    website.textContent = `${business.website}`;
+
+    logo.setAttribute('src', business.logo);
+    logo.setAttribute('alt', `Logo of ${business.name}`);
+    logo.setAttribute('loading', 'lazy');
+
+    card.appendChild(logo);
+    card.appendChild(address);
+    card.appendChild(telephone);
+    card.appendChild(website);
+
+    document.querySelector('div.cards').appendChild(card);
 }
-
-
-/* Adding the hidden date input on join page */
-
-document.querySelector("#date").innerHTML = today;
