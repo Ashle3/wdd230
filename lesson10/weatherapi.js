9b4d8204c3bed0cda95d50bb6807efc16f352e15
Ashle3 <ashleeb3@byui.edu>
2022-11-16 22:56:36 -0600

    finished weather api assignment

--- lesson10/weatherapi.js
+++ lesson10/weatherapi.js
@@ -6,27 +6,28 @@ const url = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=i
 
 async function apiFetch() {
     try {
-        const response = await fetch(url);
-        if (response.ok) {
-            const data = await response.json();
-            console.log(data);
-        } else {
-            throw Error(await response.text());
-        }
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
     } catch (error) {
         console.log(error);
     }
-}
+  }
+  
+  apiFetch();
 
-apiFetch();
-
-function displayResults(weatherData) {
-    currentTemp.innerHTML = `${weatherData.main.temp.toFixed(0)}`;
-
+function  displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+  
     const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
     const desc = weatherData.weather[0].description;
-
+  
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
     captionDesc.textContent = desc;
-}
+  }
