e01304456de0e81f2e47fd838dcb02768f7eb8a0
Ashle3 <ashleeb3@byui.edu>
2022-12-14 13:56:55 -0600

    finished home page in small view

---
+++ final/js/forecast.js
@@ -0,0 +1,1 @@
+const url = "api.openweathermap.org/data/2.5/forecast?q=Carlsbad&units=imperial&appid=1f57b19719805331049cf09ffe51943d";
--- final/js/home.js
+++ final/js/home.js
@@ -1,4 +1,22 @@
-
+/* Opens the navigation menu */
 function toggleMenu() {
     document.getElementsByClassName("nav")[0].classList.toggle("responsive");
 }
+
+/* Gets today's date, last modified */
+
+const today = new Date();
+
+try {
+    let options = {
+        year: "numeric"
+
+    }
+    document.getElementById(
+        "currentyear"
+        ).textContent = new Date().toLocaleDateString("en-US", options);
+} catch (e) {
+    alert("Error");
+}
+
+document.getElementById("lastModified").innerHTML = (document.lastModified);
