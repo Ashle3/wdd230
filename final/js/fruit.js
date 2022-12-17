0b7992aff83899e069e5422c3c59c2cdbc2fb501
Ashle3 <ashleeb3@byui.edu>
2022-12-17 16:22:16 -0600

    i got the fruit names to show

--- final/js/fruit.js
+++ final/js/fruit.js
@@ -17,6 +17,8 @@ function displayFruitOption(fruit){
 
     fruitName.textContent = `${fruit.name}`;
     checkbox.setAttribute('type', 'checkbox');
+    checkbox.setAttribute('value', fruit.name);
+    checkbox.setAttribute('name', 'fruit');
 
     fruitChoice.appendChild(checkbox);
     fruitChoice.appendChild(fruitName);
@@ -24,8 +26,48 @@ function displayFruitOption(fruit){
     document.querySelector('div.fruitopt').appendChild(fruitChoice);
 }
 
-/* Output */
+/* Output 
 
-let name1 = document.querySelector("#name");
+let name1 = document.querySelector("#nameValue");
+let btn = document.querySelector(".submitBtn");
+let nameput = document.querySelector("#name");
 
-name1.textContent = document.getElementsByName("fname");
+function displayOutput() {
+    nameput.innerHTML = name1.value;
+}
+
+btn.addEventListener('click', displayOutput);*/
+
+function displayOutput() {
+    let output = document.querySelector('.output');
+    let nameput = document.createElement('p');
+    let emailput = document.createElement('p');
+    let phoneput = document.createElement('p');
+    let instructput = document.createElement('p');
+    let fruitput = document.createElement('p');
+
+    const name1 = document.querySelector("#nameValue");
+    const email = document.querySelector("#emailValue");
+    const phone = document.querySelector("#phoneValue");
+    const instructions = document.querySelector("#specinstructions");
+    const fruitPicked = document.querySelector(".fruitopt");
+
+    nameput.textContent = `Name: ${name1.value}`;
+    emailput.textContent = `Email: ${email.value}`;
+    phoneput.textContent = `Cell Phone: ${phone.value}`;
+    instructput.textContent = `Special Instructions: ${instructions.value}`;
+
+    let fruitChecks = document.querySelectorAll('input[name="fruit"]:checked');
+    let values = [];
+    fruitChecks.forEach((fruit1) => {
+        values.push(fruit1.value);
+    })
+    fruitput.textContent = `Fruits Selected: ${values}`;
+
+    output.appendChild(nameput);
+    output.appendChild(emailput);
+    output.appendChild(phoneput);
+    output.appendChild(fruitput);
+    output.appendChild(instructput);
+}
+
