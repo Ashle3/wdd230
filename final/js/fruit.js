650cba8009003e4a0ad3a41fb76163ef570c1efb
Ashle3 <ashleeb3@byui.edu>
2022-12-17 21:01:57 -0600

    done with fresh for now

--- final/js/fruit.js
+++ final/js/fruit.js
@@ -26,36 +26,26 @@ function displayFruitOption(fruit){
     document.querySelector('div.fruitopt').appendChild(fruitChoice);
 }
 
-/* Output 
-
-let name1 = document.querySelector("#nameValue");
-let btn = document.querySelector(".submitBtn");
-let nameput = document.querySelector("#name");
-
 function displayOutput() {
-    nameput.innerHTML = name1.value;
-}
-
-btn.addEventListener('click', displayOutput);*/
-
-function displayOutput() {
-    let output = document.querySelector('.output');
-    let nameput = document.createElement('p');
-    let emailput = document.createElement('p');
-    let phoneput = document.createElement('p');
-    let instructput = document.createElement('p');
-    let fruitput = document.createElement('p');
+    const output = document.querySelector('.output');
+    const nameput = document.createElement('p');
+    const emailput = document.createElement('p');
+    const phoneput = document.createElement('p');
+    const instructput = document.createElement('p');
+    const fruitput = document.createElement('p');
+    const day = document.createElement('p');
+    let date = new Date().toLocaleDateString("en-US", options);
 
     const name1 = document.querySelector("#nameValue");
     const email = document.querySelector("#emailValue");
     const phone = document.querySelector("#phoneValue");
     const instructions = document.querySelector("#specinstructions");
-    const fruitPicked = document.querySelector(".fruitopt");
 
     nameput.textContent = `Name: ${name1.value}`;
     emailput.textContent = `Email: ${email.value}`;
     phoneput.textContent = `Cell Phone: ${phone.value}`;
     instructput.textContent = `Special Instructions: ${instructions.value}`;
+    day.textContent = `Date Ordered: ${date}`;
 
     let fruitChecks = document.querySelectorAll('input[name="fruit"]:checked');
     let values = [];
@@ -64,10 +54,26 @@ function displayOutput() {
     })
     fruitput.textContent = `Fruits Selected: ${values}`;
 
+
+   
     output.appendChild(nameput);
     output.appendChild(emailput);
     output.appendChild(phoneput);
     output.appendChild(fruitput);
     output.appendChild(instructput);
+    output.appendChild(day);
+}
+
+function displayNutrition(fruit){
+    const nutrition = document.createElement('section');
+    const carbohydrates = document.createElement('p');
+    const protein = document.createElement('p');
+    const fat = document.createElement('p');
+    const sugar = document.createElement('p');
+    const calories = document.createElement('p');
+
+    carbohydrates.textContent = `Carbohydrates: ${fruit.nutritions.carbohydrates}`;
+
+    nutrition.appendChild(carbohydrates);
 }
 
